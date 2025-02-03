1a7e0ace641e66439de0c04ccb6138f4882fc554
deanhansen <hansed3@mcmaster.ca>
2025-02-02 23:06:38 -0500

    Added code to animate population period given a geography (set to Canada currently)

--- R/population_estimates_plot.R
+++ R/population_estimates_plot.R
@@ -1,9 +1,15 @@
-library('tidyverse')
+library('dplyr')
+library('ggplot2')
 library('scales')
 library('ggthemes')
+library('gganimate')
 
 ## ...
-population_estimates <- readr::read_csv(file = "data-raw/population_estimates_data.csv")
+population_estimates <- 
+  readr::read_csv(
+    file      = "data-raw/population_estimates_data.csv", 
+    col_types = list(YEAR = "integer")
+    )
 
 ## ...
 population_filters <- 
@@ -19,59 +25,49 @@ population_estimates_filtered <-
   population_estimates |> 
   dplyr::filter(
     GEO  == population_filters$GEO,
-    YEAR == population_filters$YEAR,
+    # YEAR == population_filters$YEAR,
     GENDER    %in% population_filters$GENDER,
     AGE_GROUP %in% population_filters$AGE_GROUP_LEVELS
     ) |> 
-  dplyr::mutate(AGE_GROUP = factor(AGE_GROUP, levels = population_filters$AGE_GROUP_LEVELS)) |> 
-  dplyr::select("GENDER", "AGE_GROUP", "POPULATION_SIZE")
+  tidyr::drop_na(POPULATION_SIZE) |> 
+  dplyr::mutate(
+    AGE_GROUP       = factor(AGE_GROUP, levels = population_filters$AGE_GROUP_LEVELS),
+    POPULATION_SIZE = dplyr::if_else(GENDER == "Men", (-1) * POPULATION_SIZE, POPULATION_SIZE)
+    ) |> 
+  dplyr::select("YEAR", "GENDER", "AGE_GROUP", "POPULATION_SIZE")
 
-## ...
-geom_pyramid <- function(.data){
-  
-  ## ...
-  population_range <- range(.data$POPULATION_SIZE)
-  
-  ## ...
-  age_range_seq <- pretty(population_range, n = 10)
-  
-  .data |>
+range(population_estimates_filtered$POPULATION_SIZE)
+
+# Population Pyramid by Year and Sex --------------------------------------
+
+population_pyramid_anim <-
   ggplot2::ggplot(
-    ggplot2::aes(x = POPULATION_SIZE, y = AGE_GROUP, fill = GENDER)
+  data    = population_estimates_filtered, 
+  mapping = ggplot2::aes(x = POPULATION_SIZE, y = AGE_GROUP, fill = GENDER)
+  ) +
+  ggplot2::geom_col(width = 0.65) +
+  ggplot2::scale_x_continuous(
+    labels = function(x) scales::label_comma()(abs(x)),
+    breaks = scales::breaks_pretty(n = 9),
+    limits = c(-3e6, 3e6),
+    expand = ggplot2::expansion(mult = 0.1)
     ) +
-    ggplot2::geom_col() +
-    ggplot2::scale_x_continuous(
-      breaks  = age_range_seq,
-      labels = scales::label_comma(abs(age_range_seq))
-      ) +
-    ggplot2::scale_fill_brewer(palette = "Dark2", guide = ggplot2::guide_legend(title = "")) +
-    ggplot2::theme_minimal() +
-    ggplot2::theme(
-      axis.title.x    = ggplot2::element_blank(),
-      axis.title.y    = ggplot2::element_blank(),
-      legend.position = "top"
-      )
-  
-}
+  ggplot2::scale_fill_brewer(palette = "Dark2", type = "qual", direction = -1) +
+  ggplot2::labs(title = "Canadian Population Pyramid: {frame_time}", fill = "Sex") +
+  ggplot2::theme(
+    axis.title.x    = ggplot2::element_blank(),
+    axis.title.y    = ggplot2::element_blank(),
+    legend.position = "bottom",
+    legend.text     = ggplot2::element_text(size = 11)
+    ) +
+  ## Animation!
+  gganimate::transition_time(YEAR) +
+  gganimate::ease_aes(default = "linear") +
+  gganimate::view_follow(fixed_x = TRUE, fixed_y = TRUE)
 
 ## ...
-population_estimates |> 
-  dplyr::filter(GEO == "Canada", AGE_GROUP %in% AGE_GROUP_LEVELS, GENDER %in% c("Men", "Women"), YEAR == 2024) |> 
-  dplyr::mutate(AGE_GROUP = factor(AGE_GROUP, levels = AGE_GROUP_LEVELS)) |> 
-  ggplot2::ggplot(
-    ggplot2::aes(x = POPULATION_SIZE, y = AGE_GROUP, fill = GENDER)
-    ) + 
-  ggplot2::geom_col() +
-  ggplot2::scale_x_continuous(labels = scales::label_comma()) +
-  ggplot2::labs(fill = "Gender") +
-  ggplot2::theme(
-    axis.title.x = ggplot2::element_blank(),
-    axis.title.y = ggplot2::element_blank()
-    )
+gganimate::animate(population_pyramid_anim, duration = 5, fps = 60, height = 5, width = 7, units = "in", res = 300)
 
-
-population_estimates |> 
-  dplyr::filter(GEO == "Canada", AGE_GROUP %in% AGE_GROUP_LEVELS, GENDER %in% c("Men", "Women"), YEAR == 2024) |> 
-  dplyr::mutate(AGE_GROUP = factor(AGE_GROUP, levels = AGE_GROUP_LEVELS)) |> 
-  geom_pyramid()
+## ...
+gganimate::anim_save("man/GIF/Canada Population Pyramid.gif")
 
