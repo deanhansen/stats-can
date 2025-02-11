2187daf2850af407bfe81f758adfb7bfdbc282f3
deanhansen <hansed3@mcmaster.ca>
2025-02-11 10:41:40 -0500

    Updates to the religion_data.R file

--- R/religion/religion_data.R
+++ R/religion/religion_data.R
@@ -1,38 +1,47 @@
+## Source: https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=9810035301
+
 library('statcanR')
 library('tidyverse')
-source('statscan_utils/data_loading.R')
 
-religion_raw <- statcan_download_data("98-10-0353-01", lang = "eng")
+## ...
+religion_raw <- 
+  statcanR::statcan_download_data(tableNumber = "98-10-0353-01", lang = "eng") |> 
+  dplyr::select(-seq(9, 57, by = 2)) |>
+  dplyr::as_tibble() |>
+  dplyr::rename_with(
+    .cols = dplyr::starts_with("Religion"), 
+    .fn   = \(x) stringr::str_split_i(string = x, pattern = ":", i = 2) |> stringr::str_split_i(pattern = "\\[", i = 1)
+    ) |> 
+  dplyr::rename(
+    "AGE"        = "Age (15C)",
+    "GENDER"     = "Gender (3)",
+    "STATISTICS" = "Statistics (2)"
+  ) |> 
+  tidyr::pivot_longer(
+    cols      = "Total - Religion":"No religion and secular perspectives",
+    names_to  = "RELIGION",
+    values_to = "TOTAL_ADHERANTS"
+    ) |>
+  dplyr::select("REF_DATE", "GEO", "AGE", "GENDER",
+                "STATISTICS", "RELIGION", "TOTAL_ADHERANTS")
 
-#remove multiple columns
+## Values of `AGE` we want to keep for plotting
+AGE_VALUES <- c("0 to 14 years", "15 to 19 years", "20 to 24 years", "25 to 34 years", 
+                "35 to 44 years", "45 to 54 years", "55 to 64 years", "65 to 74 years", 
+                "75 years and over")
+
+## ...
 religion <-
   religion_raw |> 
-  select(-c(Symbol, DGUID, COORDINATE, INDICATOR, Coordinate))
-
-#update the names of the columns
-names(religion) <- 
-  religion |> 
-  names() |> 
-  snakecase::to_snake_case() |> 
-  str_replace("religion_25_", "") |> 
-  str_replace_all("[:digit:]", "") |> 
-  snakecase::to_snake_case()
-
-#filter out data for only % of population
-religion <-
-  religion |> 
-  filter(
-    statistics == "% distribution (2021)",
-    gender == "Total - Gender",
-    age_c != "Total - Age"
+  dplyr::filter(STATISTICS == "2021 Counts", STATISTICS == "2021 Counts", GENDER != "Total - Gender", AGE %in% AGE_VALUES) |> 
+  dplyr::mutate(
+    GENDER          = factor(GENDER, levels = c("Men+", "Women+"), labels = c("Men", "Women")),
+    AGE             = factor(AGE,    levels = AGE_VALUES),
+    TOTAL_ADHERANTS = as.integer(TOTAL_ADHERANTS),
+    YEAR            = lubridate::year(REF_DATE)
   ) |> 
-  select(
-    -c(gender, statistics, total_religion)
-  ) |> 
-  pivot_longer(
-    cols = 4:27, 
-    names_to = "name", 
-    values_to = "proportion"
-    )
+  dplyr::select("YEAR", "GEO", "AGE", "GENDER", 
+                "RELIGION", "TOTAL_ADHERANTS")
 
-write_csv(religion, "religion/religion.csv")
+## ...
+readr::write_csv(x = religion, file = "data-raw/religion_data.csv")
